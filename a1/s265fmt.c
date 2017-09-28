4ff0bcd30228be5da3f82f8838f808333e40725f
Taylor Eby <tjreby998@gmail.com>
2017-09-28 16:36:00 -0700

    Added ?format searching

--- a1/s265fmt.c
+++ a1/s265fmt.c
@@ -10,8 +10,24 @@ September 2017
 
 #define BUFLEN 100
 
+/*
+This struct records current options.
+-1 indicates no option is selected 
+*/
+typedef struct fmt_options {
+	/* -1 indicates feature is off */
+	int pgwdth;
+	/* Default is 0 */
+	int mrgn;
+	/* "on" == 0
+	   "off == -1 */
+	int fmt;
+} fmt_options;
+
 int main(int argc, char* argv[]) {
 	char buffer[BUFLEN];
+	char *p = buffer;
+	fmt_options options = {-1, 0, -1};
 
 	if (argc != 2) {
 		printf("Input file not specified\n");
@@ -25,6 +41,22 @@ int main(int argc, char* argv[]) {
 	}
 
 	while (fgets(buffer, sizeof(char)*BUFLEN, fp)) {
+		/* May not cover cases of ?fmt embedded in text */
+		if (*p == '?') {
+			//printf("FORMAT ME!\n");
+			if (strncmp(buffer, "?pgwdth", 7) == 0) {
+				// Temp value
+				options.pgwdth = 20;
+				options.fmt = 0;
+				printf("FOUND PGWDTH\n");
+			} else if (strncmp(buffer, "?mrgn", 5) == 0) {
+				printf("FOUND MRGN\n");
+				// Implement Me!
+			} else if (strncmp(buffer, "?fmt", 4) == 0) {
+				printf("FOUND FMT\n");
+				// Implement Me!
+			}
+		}
 		printf("%s", buffer);
 	}
 
