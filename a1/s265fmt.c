c75d45faf2d3531545b160701e90848e4da7a546
Taylor Eby <tjreby998@gmail.com>
2017-09-28 15:44:05 -0700

    Verified file reading with printf

--- a1/s265fmt.c
+++ a1/s265fmt.c
@@ -8,22 +8,27 @@ September 2017
 #include <stdlib.h>
 #include <string.h>
 
-int main(int argc, char* argv[])
-{
-	if( argc != 2 ) {
+#define BUFLEN 100
+
+int main(int argc, char* argv[]) {
+	char buffer[BUFLEN];
+
+	if (argc != 2) {
 		printf("Input file not specified\n");
 		exit(1);
 	}
 
 	FILE *fp = fopen(argv[1], "r");
-	if( fp == NULL ) {
+	if (fp == NULL) {
 		printf("Input file doesn't exist\n");
 		exit(1);
 	}
 
-	fclose(fp);
+	while (fgets(buffer, sizeof(char)*BUFLEN, fp)) {
+		printf("%s", buffer);
+	}
 
-	printf("Hello World\n");
+	fclose(fp);
 	return 0;
 }
 
