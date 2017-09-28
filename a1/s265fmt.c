af297d3f0ced4d346b26548b64159673d879ee03
Taylor Eby <tjreby998@gmail.com>
2017-09-28 15:39:14 -0700

    Added file opening/closing.

--- a1/s265fmt.c
+++ a1/s265fmt.c
@@ -4,11 +4,25 @@ Taylor Eby
 September 2017
 */
 
-#include "stdio.h"
-#include "string.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[])
 {
+	if( argc != 2 ) {
+		printf("Input file not specified\n");
+		exit(1);
+	}
+
+	FILE *fp = fopen(argv[1], "r");
+	if( fp == NULL ) {
+		printf("Input file doesn't exist\n");
+		exit(1);
+	}
+
+	fclose(fp);
+
 	printf("Hello World\n");
 	return 0;
 }
