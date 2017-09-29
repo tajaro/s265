67419fdf9d5ddbb7d4ecbc3d9ec85be2e9281f7f
Taylor Eby <tjreby998@gmail.com>
2017-09-29 09:42:27 -0700

    Added .gitignore to a1. Also s265fmt sorta working, but need to add tokenization for tabs, not just spaces

--- a1/s265fmt.c
+++ a1/s265fmt.c
@@ -30,6 +30,7 @@ int main(int argc, char* argv[]) {
 	char line[MAX_LINE_LEN];
 	char words[MAX_WORDS][MAX_WORD_LEN];
 	int num_words = 0;
+	int num_chars;
 	fmt_options options = {0, 0, 0};
 
 	if (argc != 2) {
@@ -49,19 +50,18 @@ int main(int argc, char* argv[]) {
 		make formatting easier*/
 		// fxn? void tokenize_line(char *line, char *words))
 		char *t;
-		t = strtok(line ," ");
+		t = strtok(line ,'?');
 		while (t != NULL) {
-			num_words++;
 			strncpy(words[num_words], t, MAX_WORD_LEN);
-			t = strtok(NULL, " ");
-			
+			t = strtok(NULL, " ");	
+	
 			// fxn? void update_options (fmt_options *options, char* word))
 			if (words[num_words][0] == '?') {
 				//printf("FORMAT ME!\n");
 				if (strncmp(words[num_words], "?pgwdth", 7) == 0) {
 					printf("FOUND PGWDTH\n");
 					// Temp value
-					options.pgwdth = 20;
+					options.pgwdth = 30;
 					options.fmt = 1;
 				} else if (strncmp(words[num_words], "?mrgn", 5) == 0) {
 					printf("FOUND MRGN\n");
@@ -76,44 +76,29 @@ int main(int argc, char* argv[]) {
 					// Temp value
 					options.fmt = 0;
 				}
-			}
-		}			
+			} else {
 
-		/*
-		// fxn? void update_options (fmt_options *options, char* line))
-		if (word[0] == '?') {
-			//printf("FORMAT ME!\n");
-			if (strncmp(buffer, "?pgwdth", 7) == 0) {
-				printf("FOUND PGWDTH\n");
-				// Temp value
-				options.pgwdth = 20;
-				options.fmt = 1;
-			} else if (strncmp(buffer, "?mrgn", 5) == 0) {
-				printf("FOUND MRGN\n");
-				// Temp value
-				options.mrgn = 20;
-			} else if (strncmp(buffer, "?fmt on", 7) == 0) {
-				printf("FOUND FMT ON\n");
-				// Temp value
-				options.fmt = 1;
-			} else if (strncmp(buffer, "?fmt off", 8) == 0) {
-				printf("FOUND FMT OFF\n");
-				// Temp value
-				options.fmt = 0;
+				if (options.fmt == 1) {
+					num_chars += strlen(words[num_words]);
+					if (num_chars >= options.pgwdth) {
+						printf("\n");
+						num_chars = 0;
+					}
+					printf("%s ", words[num_words]);
+				} else if (options.fmt == 0) {
+					// Implement me!
+					printf("FORMATTING IS OFF!\n");
+				}
 			}
-		}
-		*/
+			
+			num_words++;
+		} /* end of tokenization loop */			
+		
+		printf("%s", line);
 
-		// fxn? void format_line (fmt_options *options, char *buffer)
-		if (options.fmt = 1) {
-			if (options.pgwdth != 0) {
-				// Implement me!
-			} else if (options.mrgn != 0) {
-				// Implement me!
-			}
-		}
 		/* reset words array before end of loop */
 		num_words = 0; 
+		
 	}
 
 	fclose(fp);
