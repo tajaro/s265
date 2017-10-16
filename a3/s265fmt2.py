9f9a93eb8036bdd510be5fc5865bb501dd0ba6ff
Taylor Eby <tjreby998@gmail.com>
2017-10-16 13:21:52 -0700

    Added filename parsing

--- a3/s265fmt2.py
+++ a3/s265fmt2.py
@@ -5,7 +5,15 @@ import argparse
 from formatting import seng265_formatter
 
 def main():
-	#parser = argparse.ArgumentParser()
+	parser = argparse.ArgumentParser()
+	parser.add_argument('filename', nargs='?', help='file to be processeed')
+
+	args = parser.parse_args()
+
+	if not(args.filename):
+		print('No file specified')
+		sys.exit(1)
+
 	#seng265_formatter(
 	print ("Hello World")
 
